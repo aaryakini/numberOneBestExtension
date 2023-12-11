7678693dc10ea017f542efe2d40e31e252d14309
shravanibagagwde <bagas557@newschool.edu>
2023-12-11 03:57:00 -0500

    few name changes

--- darkMode/darkMode.js
+++ darkMode/darkMode.js
@@ -70,7 +70,7 @@ let chumboxes = document.querySelector('.chumboxes');
         // window.open(link, '_blank',`toolbar=yes,scrollbars=yes,resizable=yes,top=${randomTop},left=${randomLeft},width=750,height=500`)
 
     }  
-    setInterval(() => {printRandomBlock();}, 1000);
+    setInterval(() => {printRandomBlock();}, 7500);
 
 });
 
